7bf1c99ed53d422c65508a77753f51f53aec38ad
Po-Tung <berton1679@gmail.com>
2013-12-18 12:55:17 +0800

    Evaluation_V1

--- src/com/fbergeron/ai/AI.java
+++ src/com/fbergeron/ai/AI.java
@@ -1,5 +1,6 @@
 package com.fbergeron.ai;
 
+import java.util.*;
 import com.fbergeron.card.Stack;
 import com.fbergeron.solitaire.GameState;
 import com.fbergeron.solitaire.SequentialStack;
@@ -28,31 +29,49 @@ public class AI{
 		SequentialStack[] seqStack = gs.getSeqStack();
         SolitaireStack s1;
         SolitaireStack s2;
-        ClassicCard card1;
+        ClassicCard card1; 
         ClassicCard card2;
+        
 		int eval = 0;
         int minDist;
         int numDown = 0;
         int ratio = 100;
+        int[] head = new int [Solitaire.SOL_STACK_CNT];
+		for(int i=0;i<Solitaire.SOL_STACK_CNT;i++){
+            s1 = solStack[i];
+            if(s1.isEmpty()){
+                head[i] = -1;
+                continue;
+            }
+            for(int k=0;k<s1.cardCount();k++)
+                if(s1.elementAt(k).isFaceDown()) numDown++;
+                else{
+                    head[i] = k;
+                    break;
+                }
+        }
 		for(int i=0;i<Solitaire.SOL_STACK_CNT;i++){
             minDist = 14;
             s1 = solStack[i];
-            card1 = (ClassicCard)s1.top();
-            if(card1==null) continue;
-            for(Card  c:s1)
-                if(c.isFaceDown()) numDown++;
-		    for(int j=i+1;j<Solitaire.SOL_STACK_CNT;j++){
+            if(s1.isEmpty()) continue;
+//            card1 = (ClassicCard)s1.top();
+            card1 = (ClassicCard)s1.elementAt(head[i]);
+            System.out.println("Card1 "+s1+" "+card1);
+		    for(int j=0;j<Solitaire.SOL_STACK_CNT;j++){
                 s2 = solStack[j];
+                if(s2.isEmpty()) continue;
                 card2 = (ClassicCard)s2.top();
-                if(card2==null) continue;
-                if(card1.equals(card2)) continue;
-                boolean gate1 = (Math.abs(card1.getValue().getValue()-card2.getValue().getValue())/2)==0;
+                if(card1.getValue().getValue()>=card2.getValue().getValue()) continue;
+//                card2 = (ClassicCard)s2.elementAt(head[j]);
+                boolean gate1 = (Math.abs(card1.getValue().getValue()-card2.getValue().getValue())%2)==1;
                 boolean gate2 = card1.getColor()==card2.getColor();
                 if(gate1^gate2){
+                    System.out.println("Card2 "+s2+" "+card2);
                     int dist = Math.abs(card1.getValue().getValue()- card2.getValue().getValue());
                     if(dist <minDist)  minDist = dist;
                 }
             }
+            if(minDist==14) minDist = 0;
             eval = eval + minDist;
 		}
         eval = eval + ratio*(numDown);
