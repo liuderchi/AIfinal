1fc80f1e85258682d15c972a11eca98b037d6dd9
Po-Tung <berton1679@gmail.com>
2013-12-18 11:08:10 +0800

    EvaluationFaceDownProblem

--- src/com/fbergeron/ai/AI.java
+++ src/com/fbergeron/ai/AI.java
@@ -4,7 +4,9 @@ import com.fbergeron.card.Stack;
 import com.fbergeron.solitaire.GameState;
 import com.fbergeron.solitaire.SequentialStack;
 import com.fbergeron.solitaire.SolitaireStack;
+import com.fbergeron.solitaire.Solitaire;
 import com.fbergeron.card.ClassicCard;
+import com.fbergeron.card.Card;
 
 public class AI{
 	private GameState currState;
@@ -24,19 +26,36 @@ public class AI{
 		Stack revealedCards = gs.getRevealedCards();
 		SolitaireStack[] solStack = gs.getSolStack();
 		SequentialStack[] seqStack = gs.getSeqStack();
+        SolitaireStack s1;
+        SolitaireStack s2;
         ClassicCard card1;
         ClassicCard card2;
 		int eval = 0;
-        int minDist = 14;
-		for(SolitaireStack s1:solStack){
+        int minDist;
+        int numDown = 0;
+        int ratio = 100;
+		for(int i=0;i<Solitaire.SOL_STACK_CNT;i++){
+            minDist = 14;
+            s1 = solStack[i];
             card1 = (ClassicCard)s1.top();
-            System.out.println(card1);
-            for(SolitaireStack s2:solStack){
+            if(card1==null) continue;
+            for(Card  c:s1)
+                if(c.isFaceDown()) numDown++;
+		    for(int j=i+1;j<Solitaire.SOL_STACK_CNT;j++){
+                s2 = solStack[j];
                 card2 = (ClassicCard)s2.top();
-                System.out.println(card2);
+                if(card2==null) continue;
+                if(card1.equals(card2)) continue;
+                boolean gate1 = (Math.abs(card1.getValue().getValue()-card2.getValue().getValue())/2)==0;
+                boolean gate2 = card1.getColor()==card2.getColor();
+                if(gate1^gate2){
+                    int dist = Math.abs(card1.getValue().getValue()- card2.getValue().getValue());
+                    if(dist <minDist)  minDist = dist;
+                }
             }
-            
+            eval = eval + minDist;
 		}
+        eval = eval + ratio*(numDown);
 		System.out.println("eval = "+ eval);
 		return eval;
 	}
